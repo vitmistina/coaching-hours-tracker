792ac2bfc68c2b33f4e7bf7abad3a655f1541b3c
Vit Mistina <vit.mistina@gmail.com>
2019-02-02 19:23:35 +0100

    start on chart

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,12 +1,48 @@
 import React, { Component } from "react";
+import dateFns from "date-fns";
+import { Line } from "react-chartjs-2";
 import TopNavigation from "./TopNavigation";
+import base, { firebaseApp } from "../base";
 
 class Tracker extends Component {
+  componentDidMount() {
+    const uid = localStorage.getItem(`uid`);
+    if (uid) {
+      this.ref = base.syncState(`${uid}/meetings`, {
+        context: this,
+        state: "meetings"
+      });
+    }
+  }
+
+  startDate = new Date("2019-01-29");
+  endDate = new Date("2019-02-25");
+  today = new Date();
+  dateDifference = dateFns.differenceInDays(this.endDate, this.startDate);
+
+  targetLine = Array.from(
+    new Array(this.dateDifference),
+    (val, index) => this.dateDifference * 2 - 2 * index - 2
+  );
+
+  data = {
+    labels: Array.from(new Array(this.dateDifference), (val, index) =>
+      dateFns.format(dateFns.addDays(this.startDate, index), "DD.MM.")
+    ),
+    datasets: [
+      {
+        label: "Target",
+        fill: true,
+        data: this.targetLine
+      }
+    ]
+  };
+
   render() {
     return (
       <div>
         <TopNavigation history={this.props.history} />
-        Tracker
+        <Line data={this.data} />
       </div>
     );
   }
